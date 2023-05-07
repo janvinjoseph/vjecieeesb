5a05a8eab719804962ea90675fef6b78673105d0
Vyshnav Sreeshan <vyshnav.sreeshan05@gmail.com>
2023-05-07 14:02:22 +0530

    added the section for society

---
+++ src/Society/Society.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Society.css'
+function Society(){
+    return(
+        <div className="society">
+            <h1>this is the Society section</h1>
+        </div>
+    )
+}
+
+export default Society;
