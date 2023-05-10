a88150c4ee620c8a06c8b858cbe6b09a1a0eead9
1blackghost <ashishjosephnew@gmail.com>
2023-05-10 13:41:09 +0530

    done almost the skelton of societies

--- src/Society/Society.js
+++ src/Society/Society.js
@@ -1,9 +1,137 @@
 import React from "react";
 import './Society.css'
+
 function Society(){
     return(
-        <div className="society">
-            <h1>this is the Society section</h1>
+        <div className="society-container">
+            <h1 className="main-heading">IEEE Societies</h1>
+            <div className="society">
+                <h2 className="society-heading">SB</h2>
+                <div class="society-image">
+                  <img src="background.jpg" alt="Image 1" />
+                  <img src="background.jpg" alt="Image 2" />
+                </div>
+
+
+                <p className="society-text">
+                    An IEEE Student Branch gives students a community of peers, and a connection to faculty and industry professionals who drive innovation in countless technical fields. Student involvement in Branch activities, whether special projects, social and technical meetings, outreach programs, conferences, local Section or Regional opportunities, etc. can help develop a record of accomplishment and capabilities beyond the norm.
+                </p>
+            </div>
+        <div class="society">
+            <h2 class="society-heading">IEEE Industry Applications Society (IAS)</h2>
+            <img src="background.jpg" className="society-image"></img>
+
+            <div class="society-subsection">
+                <h3 class="subsection-heading">IAS Vision Statement</h3>
+                <p>IEEE Industry Applications Society will be a world leader in the advancement of science and technology linking theory and practice in the application of electrical and electronic systems for the benefit of humanity.</p>
+            </div>
+            <div class="society-subsection">
+                <h3 class="subsection-heading">IAS Mission Statement</h3>
+                <p>IEEE Industry Applications Society enables the advancement of theory and practice in the design, development, manufacturing and application of safe, sustainable, reliable, smart electrical systems, equipment and services.</p>
+            </div>
+            <div class="society-subsection">
+                <h3 class="subsection-heading">What We Value</h3>
+                <ul>
+                    <li>The advancement of theory and practice of electrical and electronic engineering for the benefit of humanity</li>
+                    <li>The promotion of safe, reliable, sustainable and economical installations</li>
+                    <li>The sharing of knowledge in our domains</li>
+                    <li>The creation of engineering standards and recommended practices</li>
+                    <li>The professional development of our global membership</li>
+                </ul>
+            </div>
+        </div>
+            <div className="society">
+                <h2 className="society-heading">WIE</h2>
+                <img src="background.jpg" className="society-image"></img>
+
+                <p className="society-text">
+
+                    IEEE Women in Engineering (WIE) is a global network of IEEE members and volunteers dedicated to promoting women engineers and scientists, and inspiring girls around the world to follow their academic interests in a career in engineering and science.
+
+                </p>
+            </div>
+            <div className="society">
+                <h2 className="society-heading">RAS</h2>
+                <img src="background.jpg" className="society-image"></img>
+
+                <p className="society-text">
+
+                    The IEEE Robotics and Automation Society's objectives are scientific, literary and educational in character. The Society strives for the advancement of the theory and practice robotics and automation engineering and science and of the allied arts and sciences, and for the maintenance of high professional standards among its members, all in consonance with the Constitution and Bylaws of the IEEE and with special attention to such aims within the Field of Interest of the Society.
+
+                Our Mission is to foster the development and facilitate the exchange of scientific and technological knowledge in Robotics and Automation that benefits members, the profession and humanity.
+                Our Vision is to be the most recognized and respected global organization in Robotics and Automation. 
+
+                </p>
+            </div>
+            <div className="society">
+                <h2 className="society-heading">PES</h2>
+                <img src="background.jpg" className="society-image"></img>
+
+                <p className="society-text">
+
+                      IEEE Power & Energy Society (PES) is the leading provider of the latest technological developments in the power and energy industry, the formation of key industry standards, and the continuing education of society members, industry, and the general public.
+                </p>
+            </div>
+            <div class="society">
+                <h2 class="society-heading">Power Electronics Society (PELS)</h2>
+                <img src="background.jpg" class="society-image"></img>
+
+                <div class="society-subsection">
+                    <h3 class="subsection-heading">About PELS</h3>
+                    <p>The Power Electronics Society (PELS) is one of the fastest-growing technical societies of the Institute of Electrical and Electronics Engineers (IEEE). For over 35 years, PELS has facilitated and guided the development and innovation in power electronics technology. This technology encompasses the effective use of electronic components, the application of circuit theory and design techniques, and the development of analytical tools for efficient conversion, control, and conditioning of electric power. PELS has distinguished award winners, practitioners, and preeminent researchers among its members. </p>
+                </div>
+                
+                <div class="society-subsection">
+                    <h3 class="subsection-heading">PELS Mission Statement</h3>
+                    <p>PELS is dedicated to upholding the vital scientific and educational aspects of power electronics and their applications, and to keeping its members around the globe up to date on state-of-the-art technological developments and advances in power electronics research.</p>
+                </div>
+                
+                <div class="society-subsection">
+                    <h3 class="subsection-heading">IEEE Transactions on Power Electronics</h3>
+                    <p>PELS publishes the IEEE Transactions on Power Electronics (TPEL), a top referenced journal among all IEEE publications.</p>
+                </div>
+                
+                <div class="society-subsection">
+                    <h3 class="subsection-heading">PELS Goals</h3>
+                    <p>PELS strives to build knowledge and awareness of the latest technologies and other advances in power electronics, with the goal of keeping its members current and competitive in the workplace and providing them with the tools necessary to help them grow both personally and professionally.</p>
+                </div>
+                
+                <div class="society-subsection">
+                    <h3 class="subsection-heading">Join PELS</h3>
+                    <p>We invite you to join us and benefit from a world of invaluable information and support.</p>
+                </div>
+            </div>
+
+            <div className="society">
+                <h2 className="society-heading">CS</h2>
+                <img src="background.jpg" className="society-image"></img>
+
+                <p className="society-text">
+                    Engaging computer engineers, scientists, academia, and industry professionals from all areas of computing, the IEEE Computer Society (CS) sets the standard for the education and engagement that fuels continued global technological advancement. Through conferences, publications, and programs, and by bringing together computer science and engineering leaders at every phase of their career for dialogue, debate, and collaboration, IEEE CS empowers, shapes, and guides the future of not only its members, but the greater industry, enabling new opportunities to better serve our world.
+                </p>
+            </div>
+            <div className="society">
+                <h2 className="society-heading">PHOTONICS</h2>
+                <img src="background.jpg" className="society-image"></img>
+
+                <p className="society-text">
+                    The IEEE Photonics Society forms the hub of a vibrant technical community of more than 100,000 professionals dedicated to transforming breakthroughs in quantum physics into the devices, systems and products to revolutionize our daily lives.
+From ubiquitous and inexpensive global communications via fiber optics, to lasers for medical and other applications, to flat-screen displays, to photovoltaic devices for solar energy, to LEDs for energy-efficient illumination, there are myriad examples of the society’s impact on the world around us.
+We organize, contribute to and participate in technical conferences, journals and other activities covering all aspects of photonics in order to share and disseminate our breakthroughs. We engage in photonics research, development, design and manufacturing, and are vital part of IEEE, the world’s largest technical professional association dedicated to advancing technology for the benefit of humanity.
+We provide our members with professional growth opportunities, publish journals, sponsor conferences and support local chapter and student activities around the world.
+
+                </p>
+            </div>
+            <div className="society">
+                <h2 className="society-heading">CASS</h2>
+                <img src="background.jpg" className="society-image"></img>
+
+                <p className="society-text">
+                    The IEEE Circuits and Systems Society is the leading organization that promotes the advancement of the theory, analysis, design, tools, and implementation of circuits and systems. The field spans their theoretical foundations, applications, and architectures, as well as circuits and systems implementation of algorithms for signal and information processing.
+The Society brings engineers, researchers, scientists and others involved in circuits and systems applications access to the industry’s most essential technical information, networking opportunities, career development tools and many other exclusive benefits.
+
+                </p>
+            </div>
         </div>
     )
 }
